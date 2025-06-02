5f182f586ddf53e5d7f0f5f11aa4338e9e614b2f
Sear Clímaco <sromero.netplus@gmail.com>
2025-06-02 17:11:34 -0600

    Authentication via sanctum

--- app/Http/Controllers/v1/management/Auth/AuthController.php
+++ app/Http/Controllers/v1/management/Auth/AuthController.php
@@ -3,14 +3,31 @@
 namespace App\Http\Controllers\v1\management\Auth;
 
 use App\Http\Controllers\Controller;
-use Carbon\Carbon;
+use http\Env\Response;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use App\Http\Requests\v1\Management\Auth\AuthRequest;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 
 class AuthController extends Controller
 {
-    public function authenticate(Request $request): JsonResponse
+    public function authenticate(AuthRequest $request): JsonResponse
     {
-        return response()->json(['Hello' => Carbon::now()->toDateTimeString()]);
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password, 'status_id' => 't'])) {
+            $user = Auth::user();
+            $token = $user->createToken('authToken')->plainTextToken;
+            $message = 'Authenticated successfully';
+
+            return response()->json([
+                'message' => $message,
+                'user' => $user,
+                'token' => $token
+            ], SymfonyResponse::HTTP_OK);
+        }
+
+        return response()->json([
+            'message' => 'Invalid credentials or inactive user',
+        ], SymfonyResponse::HTTP_UNAUTHORIZED);
     }
 }
