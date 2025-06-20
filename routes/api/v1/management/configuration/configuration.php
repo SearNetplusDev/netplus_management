65e32b48a76c854b878f57e41e786ec5017621b4
Sear Clímaco <sromero.netplus@gmail.com>
2025-06-20 17:03:46 -0600

    Agregando rutas v1/configuracion a sanctum

--- routes/api/v1/management/configuration/configuration.php
+++ routes/api/v1/management/configuration/configuration.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\v1\management\configuration\ConfigurationController;
 use App\Http\Controllers\v1\management\configuration\menu\MenuController;
 
-Route::prefix('v1/configuration')/*->middleware(['auth:sanctum'])*/ ->group(function () {
+Route::prefix('v1/configuration')->middleware(['auth:sanctum'])->group(function () {
     Route::group(['prefix' => 'menu'], function () {
         Route::get('/', [ConfigurationController::class, 'getMenu']);
         Route::post('data', [MenuController::class, 'data']);
